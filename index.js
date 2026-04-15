e9997698702542a5ac4554b04acee04d722543d2
christoffa <48928523+christoffa@users.noreply.github.com>
2026-04-15 20:58:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
 
 app.post("/generate-preview", async (req, res) => {
   try {
-    const { text } = req.body;
+    const { text } = " ";//req.body;
 
     const prompt = `
 Create ONE single modern comic-style cartoon image.
