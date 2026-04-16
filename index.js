81691b8655b871de0dabfd2e48acc8401044df96
christoffa <48928523+christoffa@users.noreply.github.com>
2026-04-16 18:13:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,29 +15,7 @@ app.use(cors());
 //app.use(express.json());
 app.use(express.json({ limit: "10mb" }));
 
-app.post(
-  "/upload-user-image",
-  upload.fields([
-    { name: "image", maxCount: 1 }
-  ]),
-  async (req, res) => {
-    try {
-      const uploadedImage = req.files["uploadedImage"][0];
-      const { text } = req.body;
-      const image_url = req.body?.image_url || "";
 
-      // 👉 These are buffers
-      console.log(uploadedImage.buffer);
-      //upload to Cloudinary
-      const uploadedURL = await cloudinary.uploader.upload(file, { upload_preset: "profile_photos" });
-
-      res.json({ ok: true });
-    } catch (err) {
-      console.error(err);
-      res.status(500).send("error");
-    }
-  }
-);
 //
 
 /*
