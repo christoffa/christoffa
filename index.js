48fb9ff8095527c03b259cc0ede375993d150ddd
christoffa <48928523+christoffa@users.noreply.github.com>
2026-04-16 16:34:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ cloudinary.config({
 const app = express();
 app.use(cors());
 //app.use(express.json());
-app.use(express.json({ limit: "15mb" }));
+//app.use(express.json({ limit: "15mb" }));
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
 
 app.post(
   "/upload-user-image",
