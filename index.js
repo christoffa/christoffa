906d7790e53e24299c336f1677411b829f7313e7
christoffa <48928523+christoffa@users.noreply.github.com>
2026-04-16 22:51:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ app.post(
      
       //SEND TO OpenAI
       const result = await openai.images.edit({
-      model: "gpt-image-1",
+      model: "gpt-image-1.5",
       n: 3,
       input: [
         {
