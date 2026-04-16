b2a2c764117228eaf95e88bc69e03bf86d62971a
christoffa <48928523+christoffa@users.noreply.github.com>
2026-04-16 16:30:37 +0100

    Update index.js
    increase json input to 15M

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ cloudinary.config({
 const app = express();
 app.use(cors());
 //app.use(express.json());
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "15mb" }));
 
 app.post(
   "/upload-user-image",
