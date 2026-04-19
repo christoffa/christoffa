58b4e15b8788bdfc5108a7ea7276e7da41695e1d
christoffa <48928523+christoffa@users.noreply.github.com>
2026-04-20 00:00:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ app.post(
     form.append("prompt", prompt);
     form.append("n", "3");
     form.append("size", "512x512");
-    form.append("image", req.files.image);
+    form.append("image", req.files.image.[0]);
 
     const response = await fetch("https://api.openai.com/v1/images/edits", {
       method: "POST",
