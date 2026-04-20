c0d3b469cec1dea4e69b28183137dc02ce9912cb
christoffa <48928523+christoffa@users.noreply.github.com>
2026-04-20 21:45:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ app.post(
 
     // Nano Banana 2 returns image data which you can then save or process
     const result2 = response.candidates[0].content.parts;//response.artifacts[0]; 
-    fs.writeFileSync("output.png", Buffer.from(result, "image/jpeg"));
+    fs.writeFileSync("output.png", Buffer.from(result2, "image/jpeg"));
 
     console.log("Image generated successfully!");
   } catch (error) {
@@ -256,7 +256,7 @@ const raw = JSON.stringify({
     // NEED UPDATE FOR MODEL
     //upload result images to cloudinary
       const uploads = await Promise.all(
-      result2.data.map(img =>
+      result2.map(img =>
         cloudinary.uploader.upload(
           `data:image/jpeg;base64,${img.b64_json}`,
           { folder: "toffa/previews" }
