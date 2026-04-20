bcdd96d1d1b579f973a80753dcc0d257849764df
christoffa <48928523+christoffa@users.noreply.github.com>
2026-04-20 21:22:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -170,7 +170,7 @@ app.post(
     
 
     // Nano Banana 2 returns image data which you can then save or process
-    const artifact = response.candidates[0].content.parts[0].inlineData.data;//response.artifacts[0]; 
+    const artifact = response.candidates[0].content;//response.artifacts[0]; 
     fs.writeFileSync("output.png", Buffer.from(artifact.base64, "base64"));
 
     console.log("Image generated successfully!");
