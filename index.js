3f106416998a0de795d2e08660f2081bf7ec6c94
christoffa <48928523+christoffa@users.noreply.github.com>
2026-04-27 20:44:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import fetch from "node-fetch";
 import FormData from "form-data";
 const upload = multer({ storage: multer.memoryStorage() });
 import { v2 as cloudinary } from "cloudinary";
+import GoogleGenerativeAI from "@google/generative-ai";
+
 //
 //const express = require("express"); 
 //const multer = require("multer"); 
@@ -20,7 +22,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
 // GOOGLE BITS
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+//const { GoogleGenerativeAI } = require("@google/generative-ai");
 const sharp = require("sharp");
 const genai = new GoogleGenerativeAI(process.env.GEM_API_KEY);
 
