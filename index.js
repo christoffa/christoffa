e396e05373951caffd0178500fc73b9a48ed1399
christoffa <48928523+christoffa@users.noreply.github.com>
2026-04-28 23:08:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -764,14 +764,14 @@ Return results in JSON format.`;
     const text = result.response.text();
     if (text) {
       console.log(JSON.stringify(JSON.parse(text), null, 2));
-    res.status(200).json({ sucess: true, data: JSON.stringify(JSON.parse(text), null, 2)});
+    res.status(200).json({ success: true, data: text});
     } else {
       console.log("No response text received.");
-    res.status(500).json({ sucess: false, errorMessage: error.message});
+    res.status(500).json({ success: false, errorMessage: error.message});
     }
   } catch (error) {
     console.error("Error during analysis:", error.message);
-    res.status(500).json({ sucess: false, error: "Error during analysis:",errorMessage: error.message});
+    res.status(500).json({ success: false, error: "Error during analysis",errorMessage: error.message});
         
   }
 });
