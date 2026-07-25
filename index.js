2edb414e2ec9c473b2a19eb79b9e1619f5b20a94
christoffa <48928523+christoffa@users.noreply.github.com>
2026-07-25 13:23:41 +0100

    Add static file serving for public directory

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ let progreesMessage = "";
 //const upload = multer({ storage: multer.memoryStorage() }); 
 //const sharp = require("sharp");
 
+
 const app = express();
 //
 cloudinary.config({
@@ -560,6 +561,7 @@ app.use(cors({
 }));
 */
 app.use(express.json({ limit: "10mb" }));
+app.use(express.static('public'));
 
 /*****************************************************/
 const uploadImage = async (base64) => {
