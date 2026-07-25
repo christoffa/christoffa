2e66a11c1af1bef7530e3c5218aa93ab3b63adb8
christoffa <48928523+christoffa@users.noreply.github.com>
2026-07-25 12:28:36 +0100

    Update file name for BAHA User Group page

--- index.js
+++ index.js
@@ -581,7 +581,7 @@ app.get("/", (req, res) => {
 
 
 app.get('/baha', (req, res) => {
-    res.sendFile('/baha-2026.html');
+    res.sendFile('/BAHA_User_Group_2026.html');
 });
 
 /*************************************************************/
