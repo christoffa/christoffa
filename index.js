376ceea41e5f28d3b20d499d26d5c30a891b61ea
christoffa <48928523+christoffa@users.noreply.github.com>
2026-07-25 13:08:55 +0100

    Fix import statement for fileURLToPath

--- index.js
+++ index.js
@@ -583,7 +583,7 @@ app.get("/", (req, res) => {
 app.get('/baha', (req, res) => {
 
 //import path from 'path';
-import { fileURLToPath } from 'url';
+import fileURLToPath  from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
