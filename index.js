bef65a2ac17c992cd9fd585a974bea970dd5a2e6
christoffa <48928523+christoffa@users.noreply.github.com>
2026-07-25 13:25:07 +0100

    Clean up imports and comment out sendFile
    Removed unused imports and commented out sendFile line.

--- index.js
+++ index.js
@@ -584,13 +584,8 @@ app.get("/", (req, res) => {
 
 app.get('/baha', (req, res) => {
 
-//import path from 'path';
-import fileURLToPath  from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-  
-    res.sendFile(path.join(__dirname, 'public', 'BAHA_User_Group_2026.html'));
+    //res.sendFile(path.join(__dirname, 'public', 'BAHA_User_Group_2026.html'));
 });
 
 /*************************************************************/
